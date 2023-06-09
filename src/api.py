254100a7643deb8746284196558a3688cb9bd718
Pewaukee <misterkar6@gmail.com>
2023-06-08 19:52:16 -0700

    api basic func

--- src/api.py
+++ src/api.py
@@ -8,4 +8,25 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 class API: # class for functionality of the api 
     def __init__(self, model:str) -> None:
         self.model = model
-        
+        # create the message list with system 
+        self.messages = []
+    def add_message(self, role:str, content:str) -> None:
+        # add a message to the message list
+        self.messages.append({"role": role, "content": content})
+    def generate_response(self, prompt:str, max_tokens:int=300, temperature:float=1) -> str: 
+        self.add_message("user", prompt)
+        # generate a OpenAI response from the prompt and update the corresponding variables
+        response = openai.ChatCompletion.create(
+            model=self.model,
+            messages=self.messages,
+            temperature=temperature,
+            max_tokens=max_tokens,
+        )
+        # add the prompt to the messages list
+        res = response['choices'][0]['message']['content']
+        self.add_message("assistant", res)
+        return res
+    
+            
+
+    
