143225c048d8b364fd2c792457e7306782689569
Pewaukee <misterkar6@gmail.com>
2023-06-09 20:17:19 -0700

    api basic config start

---
+++ data/README.md
@@ -0,0 +1,16 @@
+# Data Directory
+
+The `data` directory contains the necessary data files for the quote generation application.
+
+## File Description
+
+- `messages.txt`: This file stores the initial messages used to set up the conversation with the OpenAI API. It includes system messages that define the behavior of the assistant, user messages that specify the quote requests, and assistant messages that provide the generated quotes.
+
+## Usage
+
+The `messages.txt` file serves as a template for the conversation between the user and the assistant. It provides instructions to the assistant on how to generate quotes based on user prompts and style specifications.
+
+To modify the conversation or add more prompts, you can edit the `messages.txt` file and follow the format provided.
+
+Please note that any changes to the `messages.txt` file may impact the generated quotes and the behavior of the application.
+
--- src/api.py
+++ src/api.py
@@ -3,33 +3,28 @@
 import openai
 import tiktoken
 import os
+import json
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 class API: # class for functionality of the api 
     def __init__(self, model:str) -> None:
         self.model = model
-        # create the message list with system 
-        self.messages = [
-            {"role": "system", "content": "You are a helpful assistant that specifically works with creating quotes based of the user's prompt."},
-            {"role": "system", "content": "Specifically, you should use your creativity to make new and unique quotes."},
-            {"role": "system", "content": "Also, make sure you mimic the style of quotations of the given inspiriation."},
-            {"role": "system", "content": "For example, if the inspiration is Arnold Schwarzenegger, you should make quotes that sound like Arnold Schwarzenegger said them."},
-            {"role": "system", "content": "Add the end of your response, provide the author inspiriation from the user's prompt."},
-            {"role": "user", "content": "Give me a quote about love and marriage in the style of Arnold Schwarzenegger."},
-            {"role": "assistant", "content": "It's simple, love is like bodybuilding, the last 3-4 reps of love grow the muscle of love. - Arnold Schwarzenegger"},
-        ]
+        # create the message list with system messages from ../docs/messages.txt
+        with open('../data/messages.txt', 'r') as file:
+            json_data = file.read()
+        self.messages = json.loads(json_data)
     def add_message(self, role:str, content:str) -> None:
         # add a message to the message list
         self.messages.append({"role": role, "content": content})
-    def generate_response(self, prompt:str, max_tokens:int=300, temperature:float=1.5) -> str: # temp 1.5 to be more creative
+    def generate_response(self, prompt:str, max_tokens:int=300, temperature:float=1.0) -> str: # temp 1.5 to be more creative
         self.add_message("user", prompt)
         # generate a OpenAI response from the prompt and update the corresponding variables
         response = openai.ChatCompletion.create(
             model=self.model,
             messages=self.messages,
             temperature=temperature,
-            max_tokens=max_tokens,
+            max_tokens=max_tokens, # max tokens the API can use in its response
         )
         # add the prompt to the messages list
         response_text = response['choices'][0]['message']['content']
